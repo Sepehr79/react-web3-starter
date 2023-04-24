6be7f9dcbc8274236d6fb1241e921c327bf09df3
Sepehr79 <sepehrmsm1379@gmail.com>
2023-04-24 12:55:43 +0330

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { MetaMaskSDK } from '@metamask/sdk'
+import Web3 from "web3";
+
+const options = {
+  injectProvider: false,
+  communicationLayerPreference: 'webrtc',
+};
+const sdk = new MetaMaskSDK(options);
+const provider = sdk.getProvider()
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  if (provider.isMetaMask) {
+    let web3js = new Web3(provider)
+    // let contractInstance = new web3js.eth.Contract('CONTRACT_ABI', 'CONTRACT_ADDRESS')
+    return (
+      <div className="App">
+        <h1>Metamask is installed</h1>
+      </div>
+    );
+  } else {
+    return (
+        <h1>Please install metamask wallet</h1>
+    )
+  }
 }
 
 export default App;
